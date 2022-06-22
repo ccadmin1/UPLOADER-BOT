15c5ca895771d7b9497664cef8378c8a2b9b6d8f
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2022-06-22 11:36:42 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -42,13 +42,6 @@ async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT.format(update.from_user.mention),
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                      "channel", url="https://t.me/cinemacollections ")
-                ]
-            ]
-        ),
+
         reply_to_message_id=update.message_id
     )
